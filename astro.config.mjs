0c1c18bb210c1154bbc60901c95aaee1c17452cc
linkqb <linkqbcom@gmail.com>
2026-08-27 11:24:29 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+
+  adapter: cloudflare(),
 
   site:
     process.env.SITE_URL ||
