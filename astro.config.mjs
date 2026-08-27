15ee1aab5c068387370134ad27388071f2e1c905
linkqb <linkqbcom@gmail.com>
2026-08-27 10:32:44 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,5 @@ import { defineConfig } from 'astro';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://kode-pos.pages.dev',
-  build: {
-    format: 'directory'
-  }
+  site: 'https://kode-pos.pages.dev'
 });
